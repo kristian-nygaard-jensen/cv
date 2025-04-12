c126532bd93020bc9691d21a9e74de97e915748c
Kristian Nygaard Jensen <mail@zero-status.net>
2025-04-12 18:58:35 +0200

    Er ved at forberede noget til Rasmus

---
+++ src/main/elisp/hvad_vil_du_anbefale_?.el
@@ -0,0 +1,25 @@
+;;; hvad_vil_du_anbefale_?.el --- Description -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2025 Kristian Nygaard Jensen
+;;
+;; Author: Kristian Nygaard Jensen <kristian@zero-status.net>
+;; Maintainer: Kristian Nygaard Jensen <kristian@zero-status.net>
+;; Created: April 12, 2025
+;; Modified: April 12, 2025
+;; Version: 0.0.1
+;; Keywords: abbrev bib c calendar comm convenience data docs emulations extensions faces files frames games hardware help hypermedia i18n internal languages lisp local maint mail matching mouse multimedia news outlines processes terminals tex tools unix vc wp
+;; Homepage: https://github.com/kristian/hvad_vil_du_anbefale_?
+;; Package-Requires: ((emacs "24.3"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;;  Description
+;;
+;;; Code:
+
+
+
+(provide 'hvad_vil_du_anbefale_?)
+;;; hvad_vil_du_anbefale_?.el ends here
